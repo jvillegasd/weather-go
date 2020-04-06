5a2dc9f745f3b68f2de38f6033919e2fd80ec2d5
C9-LinkRs <linkrsfan@gmail.com>
2020-04-06 07:55:00 +0200

    Dockerized project and simple forecast almost done

---
+++ controllers/simpleForecast.js
@@ -0,0 +1,19 @@
+let express = require("express");
+
+//Libs
+let weather = require("../libs/weather");
+
+let router = express.Router();
+
+router.get("/", async (request, response) => {
+  try {
+    let cityName = request.query.city;
+    let forecast = await weather.weatherByCity(cityName);
+
+    response.status(200).json({ forecast });
+  } catch (error) {
+    response.status(500).json({ errorMessage: error });
+  }
+});
+
+module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,11 @@ let express = require("express");
 
 let router = express.Router();
 
+//Controllers
+const simpleForecast = require("../controllers/simpleForecast");
+
+router.use("/simple", simpleForecast);
+
 //Server root route
 router.get("/", (request, response) => {
   response.status(200).json({ message: "Server is up!" });
