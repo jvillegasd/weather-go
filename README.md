dcb8d2b8abf576c53a887053066b1158acc0fc26
C9-LinkRs <linkrsfan@gmail.com>
2020-04-16 06:40:00 +0200

    READMED added

---
+++ README.md
@@ -0,0 +1,92 @@
+# Weather-Go!
+
+Weather-Go is wrapper for OpenWeatherMap API. This project is inspired by [wttr](https://github.com/chubin/wttr.in).
+Weather-Go supports various information representation like JSON, emojified JSON weather and JSON-based ANSI-sequences.
+
+
+## Running it in local
+
+This project is dockerized. Run the project with the following command:
+`docker-compose up -d`
+PS: A Nginx container is used for deploy the dockerized project.
+
+## Setting Environment
+This project uses two `ENV` variables:
+*	Project exposed port: `NODEJS_PORT`
+*	OpenWeatherMap API key: `WEATHER_API_KEY`
+
+##	Usage
+Weather-Go current endpoints:
+*	Current weather
+	*	By city: `/:city`
+	*	By zipcode: `/country/:country/zip/:zip`
+	*	By coordinates: `/latitude/:latitude/longitude/:longitude`
+* Moon phase (WIP)
+* Forecast (WIP)	
+## Supported output formats
+
+Weather-Go currently supports these formats:
+* JSON
+* Emojified JSON
+
+For receive the desired format, you have to add the `format` query parameter.
+
+The JSON format was re-format from original OpenWeatherMap response.
+*JSON format input: `?format=json`.*
+**Example:**
+`{
+    "currentWeather": {
+        "coord": {
+            "lon": "-0.13",
+            "lat": "51.51"
+        },
+        "weather": {
+            "main": "Clear",
+            "description": "clear sky"
+        },
+        "temp": {
+            "celsius": {
+                "current": "4.34 °C",
+                "feelsLike": "1.26 °C",
+                "min": "2.22 °C",
+                "max": "7.22 °C"
+            },
+            "fahrenheit": {
+                "current": "39.81 °F",
+                "feelsLike": "34.27 °F",
+                "min": "36.00 °F",
+                "max": "45.00 °F"
+            },
+            "pressure": "1017 hPa",
+            "humidity": "87 %"
+        },
+        "visibility": "6.00 Km/h",
+        "wind": {
+            "speed": "7.56 Km/h",
+            "degree": "60°"
+        },
+        "country": "GB",
+        "city": "London",
+        "timezone": "1 UTC",
+        "sunrise": "4/16/2020, 06:01 AM",
+        "sunset": "4/16/2020, 07:58 PM"
+    }
+}`
+You can use the JSON emojified format feature:
+*	Format 1:
+	*	*Format input: `?format=1`.*
+	*	Response: `{ "currentWeather":  "🌑 4.43°C" }`
+*	Format 2:
+	*	*Format input: `?format=2`.*
+	*	Response: `{ "currentWeather":  "🌑 🌡️4.43°C 🌬️↗7.56 Km/h" }`
+*	Format 3:
+	*	*Format input: `?format=3`.*
+	*	Response: `{ "currentWeather":  "London: 🌑 4.28°C" }`
+*	Format 4:
+	*	*Format input: `?format=4`.*
+	*	Response: `{ "currentWeather":  "Detroit: ⛅ 🌡️-1.00°C 🌬️↓11.16 Km/h" }`
+## TODO
+
+* Implement the JSON-based ANSI-sequences format
+* Finish the Moon phase port to JavaScript
+* Forecast
--- libs/moon.js
+++ libs/moon.js
@@ -1,35 +1,33 @@
-"use strict";
-
-/* ---- Start of Atronomical constants ---- */
-
-const epoch = 2444238.5; // 1980 January 0.0
-
-/* Constants defining the Sun's apparent orbit */
-const eLonge = 278.833540; // Ecliptic longitude of the Sun at epoch 1980.0
-const elongp = 282.596403; // Ecliptic longitude of the Sun at perigee
-const eccent = 0.016718; // Eccentricity of Earth's orbit
-const sunsmax = 1.495985e8; // Semi-major axis of Earth's orbit, km
-const sunangsiz = 0.533128; // Sun's angular size, degrees, at semi-major axis distance
-
-/* Elements of the Moon's orbit, epoch 1980.0 */
-const mmlong = 64.975464; // Moon's mean longitude at the epoch
-const mmlongp = 349.383063; // Mean longitude of the perigee at the epoch
-const mlnode = 151.950429; // Mean longitude of the node at the epoch
-const minc = 5.145396; // Inclination of the Moon's orbit */
-const mecc = 0.054900; // Eccentricity of the Moon's orbit */
-const mangsiz = 0.5181; // Moon's angular size at distance a from Earth
-const msmax = 384401.0; // Semi-major axis of Moon's orbit in km
-const mparallax = 0.9507; // Parallax at distance a from Earth
-const synmonth =  29.53058868; // Synodic month (new Moon to new Moon)
-const lunatbase = 2423436.0; // Base date for E. W. Brown's numbered series of lunations (1923 January 16)
-
-/*  Properties of the Earth  */
-const earthrad = 6378.16; // Radius of Earth in kilometres
-const PI = 3.14159265358979323846;  // Assume not near black hole nor in Tennessee
-
-/* ---- End of Atronomical constants ---- */
-
 class Moon {
+
+  /* ---- Start of Atronomical constants ---- */
+
+  epoch = 2444238.5; // 1980 January 0.0
+
+  /* Constants defining the Sun's apparent orbit */
+  elonge = 278.833540; // Ecliptic longitude of the Sun at epoch 1980.0
+  elongp = 282.596403; // Ecliptic longitude of the Sun at perigee
+  eccent = 0.016718; // Eccentricity of Earth's orbit
+  sunsmax = 1.495985e8; // Semi-major axis of Earth's orbit, km
+  sunangsiz = 0.533128; // Sun's angular size, degrees, at semi-major axis distance
+
+  /* Elements of the Moon's orbit, epoch 1980.0 */
+  mmlong = 64.975464; // Moon's mean longitude at the epoch
+  mmlongp = 349.383063; // Mean longitude of the perigee at the epoch
+  mlnode = 151.950429; // Mean longitude of the node at the epoch
+  minc = 5.145396; // Inclination of the Moon's orbit */
+  mecc = 0.054900; // Eccentricity of the Moon's orbit */
+  mangsiz = 0.5181; // Moon's angular size at distance a from Earth
+  msmax = 384401.0; // Semi-major axis of Moon's orbit in km
+  mparallax = 0.9507; // Parallax at distance a from Earth
+  synmonth = 29.53058868; // Synodic month (new Moon to new Moon)
+  lunatbase = 2423436.0; // Base date for E. W. Brown's numbered series of lunations (1923 January 16)
+
+  /*  Properties of the Earth  */
+  earthrad = 6378.16; // Radius of Earth in kilometres
+  PI = 3.14159265358979323846;  // Assume not near black hole nor in Tennessee
+
+  /* ---- End of Atronomical constants ---- */
 
   constructor(dateNow) {
     this.dateNow = dateNow;
